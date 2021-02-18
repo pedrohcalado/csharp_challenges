664e5d24dc3498c36c3a1a6ae8c7acef5d3673e3
pedrohcalado <pedrocalado22@gmail.com>
2021-02-18 11:49:04 -0300

    Ajusta os testes desconsiderando o número 1 como primo

--- DividersAndPrimes/DividersAndPrimes.Test/DividersAndPrimesTest.cs
+++ DividersAndPrimes/DividersAndPrimes.Test/DividersAndPrimesTest.cs
@@ -11,18 +11,16 @@ namespace DividersAndPrimesTest
         [TestMethod]
         public void TestGetDividers_ShouldReturnListWithDividers()
         {
-            DividersAndPrimes entry = new DividersAndPrimes();
             List<int> expected = new List<int> { 1, 3, 5, 9, 15, 45 };
-            List<int> actual = entry.GetDividers(45);
+            List<int> actual = MainClass.GetDividers(45);
             Assert.AreEqual(String.Join(" ", expected), String.Join(" ", actual));
         }
 
         [TestMethod]
         public void TestGetPrimeDividers_ShouldReturnListWithPrimeDividers()
         {
-            DividersAndPrimes entry = new DividersAndPrimes();
-            List<int> expected = new List<int> { 1, 3, 5 };
-            List<int> actual = entry.GetPrimeDividers(45);
+            List<int> expected = new List<int> { 3, 5 };
+            List<int> actual = MainClass.GetPrimeDividers(45);
             Assert.AreEqual(String.Join(" ", expected), String.Join(" ", actual));
         }
     }
